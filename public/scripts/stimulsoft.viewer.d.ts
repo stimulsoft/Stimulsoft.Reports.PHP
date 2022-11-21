8daacabe59f8117465fa2cfa2b653ca3a055b366
VladimirHot <hotchenko@gmail.com>
2022-11-21 12:15:45 +0300

    new release v2022.4.5

--- public/scripts/stimulsoft.viewer.d.ts
+++ public/scripts/stimulsoft.viewer.d.ts
@@ -1,10 +1,10 @@
 /*
 Stimulsoft.Reports.JS
-Version: 2022.4.4
-Build date: 2022.11.01
+Version: 2022.4.5
+Build date: 2022.11.17
 License: https://www.stimulsoft.com/en/licensing/reports
 */
-export * from './stimulsoft.dashboards';
-import {Stimulsoft} from './stimulsoft.dashboards';
+export * from './stimulsoft.reports';
+import {Stimulsoft} from './stimulsoft.reports';
 export import StiViewer = Stimulsoft.Viewer.StiViewer;
 export import StiViewerOptions = Stimulsoft.Viewer.StiViewerOptions;
