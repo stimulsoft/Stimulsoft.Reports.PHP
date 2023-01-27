94323b6350018160b308322423aa4465524d4eab
Yuriy Kubekin <vkubekin@gmail.com>
2023-01-27 10:15:38 +0300

    new release v2023.1.6

--- src/adapters/StiMySqlAdapter.php
+++ src/adapters/StiMySqlAdapter.php
@@ -7,7 +7,7 @@ use Stimulsoft\StiResult;
 
 class StiMySqlAdapter extends StiDataAdapter
 {
-    public $version = '2023.1.5';
+    public $version = '2023.1.6';
     public $checkVersion = true;
 
     protected $driverName = 'mysql';
--- src/adapters/classes/StiDataHandler.php
+++ src/adapters/classes/StiDataHandler.php
@@ -6,7 +6,7 @@ use Stimulsoft\Adapters\StiDataAdapter;
 
 class StiDataHandler
 {
-    public $version = '2023.1.5';
+    public $version = '2023.1.6';
 
     private function stiErrorHandler($errNo, $errStr, $errFile, $errLine)
     {
